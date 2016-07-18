32ec6ae4b4386243701f0b8718b10f20b6a922a4
solverit <solver.it@gmail.com>
2016-07-18 15:41:00 +0300

    upd deps

--- project.clj
+++ project.clj
@@ -2,16 +2,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-     [org.clojure/clojure        "1.7.0"]
-     [org.clojure/clojurescript  "1.7.189"]
-     [org.clojure/core.async     "0.2.374"]
+     [org.clojure/clojure        "1.8.0"]
+     [org.clojure/clojurescript  "1.9.89"]
+     [org.clojure/core.async     "0.2.385"]
      [khroma "0.3.0"]
      [prismatic/dommy "1.1.0"]
   ]
   :source-paths ["src"]
   :profiles {:dev
              {:plugins [[com.cemerick/austin "0.1.6"]
-                        [lein-cljsbuild "1.1.1"]
+                        [lein-cljsbuild "1.1.3"]
                         [lein-chromebuild "0.3.0"]]
 
               :cljsbuild {
@@ -30,6 +30,6 @@
               }
 
              }
-  }
+      }
 
   )
